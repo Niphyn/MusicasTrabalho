6ba7a074b995a4a0bfa683d2047bf53b0177284a
Niphyn <99843031+Niphyn@users.noreply.github.com>
2022-11-17 12:43:00 -0300

    Update main.c

--- main.c
+++ main.c
@@ -121,7 +121,7 @@ int main(int argc, char **argv)
 		case (RECOMENDAR_MUSICA):
 			break;
 		case (GERAR_RELATORIO):
-			vector_gerar_relatorio(vetor_playlist,vetor_artista);
+			vector_gerar_relatorio(vetor_playlist,vetor_artista, vetor_musica);
 			break;
 		case (SAIR):
 			printf("SAINDO...\n");
